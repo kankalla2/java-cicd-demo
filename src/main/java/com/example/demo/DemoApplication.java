93ae87a0627c7499fbbbe8c3d3b623ea18d40b86
kankalla2 <kankalla@loginsoft.com>
2026-01-27 15:51:15 +0530

    Add password variable in DemoApplication
    Added a password variable for potential use.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        Runtime.getRuntime().exec(args[0]);    
+        Runtime.getRuntime().exec(args[0]);
+        String password = "admin123";
     }
 }
