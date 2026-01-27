abdaa0aaf66c7cc00b293f9a0a4beb6a38062ed2
kankalla2 <kankalla@loginsoft.com>
2026-01-27 15:57:47 +0530

    Implement user input handling in main method
    Added SQL query and file input stream for user input.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
+        String query = "SELECT * FROM users WHERE name = '" + args[0] + "'";
+        new java.io.FileInputStream(args[0]);
         SpringApplication.run(DemoApplication.class, args);
         Runtime.getRuntime().exec(args[0]);
         String password = "admin123";
